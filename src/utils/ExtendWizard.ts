5ba0e6bb28fbfd50904b1095228d69f4f09a94b8
Ibah <starlord77@mail.ru>
2024-12-26 10:21:55 +0300

    feat: добавил модификатор доступа

--- src/utils/ExtendWizard.ts
+++ src/utils/ExtendWizard.ts
@@ -1,7 +1,7 @@
 import { WizardContext } from 'telegraf/typings/scenes';
 
 export class ExtendWizard {
-  async leave(ctx: WizardContext) {
+  protected async leave(ctx: WizardContext) {
     const sceneName = ctx.scene.current.id;
     await ctx.scene.leave();
     console.log('Scene leave => '.yellow, sceneName);
