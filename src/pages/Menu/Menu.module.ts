8a3a4b1fc0eeb0820887b72dddbfbca9fe7c5947
Ibah <starlord77@mail.ru>
2024-12-28 17:47:16 +0300

    feat: add actions

--- src/pages/Menu/Menu.module.ts
+++ src/pages/Menu/Menu.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import MenuWizard from './Menu.wizard';
+import { SequelizeModule } from '@sql-tools/nestjs-sequelize';
+import { BankRequisits } from '@contact/models';
 
 @Module({
+  imports: [SequelizeModule.forFeature([BankRequisits], 'contact')],
   providers: [MenuWizard],
 })
 export default class MenuModule {}
--- src/pages/Menu/Menu.wizard.ts
+++ src/pages/Menu/Menu.wizard.ts
@@ -1,3 +1,5 @@
+import { BankRequisits } from '@contact/models';
+import { InjectModel } from '@sql-tools/nestjs-sequelize';
 import { Action, Ctx, Hears, Wizard, WizardStep } from 'nestjs-telegraf';
 import { MENU_SCENE_ID, WEBHOOK_OPERATOR_WIZARD } from 'src/app.constants';
 import { ExtendWizard } from 'src/utils/ExtendWizard';
@@ -6,7 +8,10 @@ import { WizardContext } from 'telegraf/typings/scenes';
 
 @Wizard(MENU_SCENE_ID)
 export default class MenuWizard extends ExtendWizard {
-  constructor() {
+  constructor(
+    @InjectModel(BankRequisits, 'contact')
+    private readonly modelBankRequisites: typeof BankRequisits,
+  ) {
     super();
   }
 
@@ -26,21 +31,64 @@ export default class MenuWizard extends ExtendWizard {
     );
   }
 
-  @Hears('Оплата')
-  async paymentHears(@Ctx() ctx: WizardContext) {
-    ctx.replyWithMarkdown(
-      'Каким методом вам будет провести оплату?',
-      Markup.inlineKeyboard([
-        [
-          Markup.button.callback('Реквизиты', 'requisites'),
-          Markup.button.callback('QR код', 'qrcode'),
-          Markup.button.callback('Интернет-эквайринг', 'internetAcquiring'),
-          Markup.button.callback('СБП', 'fastPaymentSystem'),
-        ],
-      ]),
-    );
+  message_id: number;
+  async deleteMessage(@Ctx() ctx: WizardContext) {
+    ctx.deleteMessage(this.message_id);
   }
 
+  @Hears('Оплата')
+  async paymentHears(@Ctx() ctx: WizardContext) {
+    ctx
+      .replyWithMarkdown(
+        'Каким методом вам будет провести оплату?',
+        Markup.inlineKeyboard([
+          [
+            Markup.button.callback('Реквизиты', 'requisites'),
+            Markup.button.callback('QR код', 'qrcode'),
+            Markup.button.callback('Интернет-эквайринг', 'internetAcquiring'),
+            Markup.button.callback('СБП', 'fastPaymentSystem'),
+          ],
+        ]),
+      )
+      .then((res) => {
+        console.log(res);
+        res.message_id = this.message_id;
+      });
+  }
+
+  @Action('requisites')
+  async requisitesAction(@Ctx() ctx: WizardContext) {
+    const requisites = await this.modelBankRequisites.findOne({
+      where: {
+        id: 8,
+      },
+    });
+    const ogrn = requisites.pay_purpose;
+    const inn = requisites.inn;
+    const kpp = requisites.kpp;
+    const r_account = requisites.r_account;
+    const k_account = requisites.k_account;
+    const bik = requisites.bik;
+    const recepient = requisites.br_name;
+    const reply = `ОГРН:${ogrn}\nИНН:${inn}\nКПП:${kpp}\nР/счёт:${r_account}\nК/счёт:${k_account}\nБИК:${bik}\nПолучатель:${recepient}`;
+    ctx.reply(reply);
+    await this.deleteMessage(ctx);
+  }
+  @Action('qrcode')
+  async qrcodeAction(@Ctx() ctx: WizardContext) {
+    ctx.reply('qrcode');
+    await this.deleteMessage(ctx);
+  }
+  @Action('internetAcquiring')
+  async internetAcquiringAction(@Ctx() ctx: WizardContext) {
+    ctx.reply('internetAcquiring');
+    await this.deleteMessage(ctx);
+  }
+  @Action('fastPaymentSystem')
+  async fastPaymentSystemAction(@Ctx() ctx: WizardContext) {
+    ctx.reply('fastPaymentSystem');
+    await this.deleteMessage(ctx);
+  }
   /** */
 
   @Hears('Сайт')
@@ -50,7 +98,10 @@ export default class MenuWizard extends ExtendWizard {
   @Hears('Оператор')
   async operatorHears(@Ctx() ctx: WizardContext) {
     await this.leave(ctx).then(() => ctx.scene.enter(WEBHOOK_OPERATOR_WIZARD));
-    ctx.reply('Перенаправляю вас на оператора');
+    const reply = `Открыт диалог с корпоративной открытой линией. 
+      \nЕсли вы хотите связаться напрямую с сотрудником, позвоните по номеру 8-(800) 222-04-01`;
+    ctx.reply(reply);
+    ctx.scene.reenter();
   }
   @Hears('Юр.информация')
   async requisitesHears(@Ctx() ctx: WizardContext) {
